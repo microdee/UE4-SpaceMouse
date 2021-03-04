e8a6870d28a6c1f6d10a7fdab0fe3c031c837962
David Morasz <david.morasz@meso.design>
2021-03-04 01:46:46 +0100

    + DebugInfoPrinter as a separate object

---
+++ Source/SpaceMouseReader/Public/DebugInfoPrinter.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Containers/StaticArray.h"
+
+struct hid_device_info;
+
+/**
+ * 
+ */
+class SPACEMOUSEREADER_API FDebugInfoPrinter
+{
+public:
+
+    static inline const int ReportCount = 4;
+    
+    FDebugInfoPrinter();
+    ~FDebugInfoPrinter();
+
+    TStaticArray<FString, ReportCount> Reports;
+    FString ReportAll {};
+    
+    void Print(FString FriendlyDeviceName, const hid_device_info* DeviceInfo, int InternalID);
+    void AppendReport(const uint8* Report, uint32 Size);
+    void SetReport(int Index, const uint8* Report, uint32 Size);
+};
