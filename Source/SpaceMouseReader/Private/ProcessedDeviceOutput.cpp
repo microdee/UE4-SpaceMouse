6924d4f2928fce26eaa1d995a8089db0af44d2bb
David Morasz <david.morasz@meso.design>
2021-03-04 01:46:24 +0100

    + device output as struct

---
+++ Source/SpaceMouseReader/Private/ProcessedDeviceOutput.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProcessedDeviceOutput.h"
+
+FProcessedDeviceOutput FProcessedDeviceOutput::operator+(const FProcessedDeviceOutput& other)
+{
+    return {
+        Translation + other.Translation,
+        Rotation + other.Rotation,
+        Buttons | other.Buttons
+    };
+}
---
+++ Source/SpaceMouseReader/Public/ProcessedDeviceOutput.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Containers/StaticBitArray.h"
+
+/**
+ * 
+ */
+struct SPACEMOUSEREADER_API FProcessedDeviceOutput
+{
+    FVector Translation {0,0,0};
+    FRotator Rotation {0,0,0};
+    TStaticBitArray<64> Buttons;
+
+    FProcessedDeviceOutput operator+(const FProcessedDeviceOutput& other);
+};
