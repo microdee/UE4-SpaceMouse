6df60c1269e6709a1553ba70f9d05c8e7ee54d7b
David Morasz <david.morasz@meso.design>
2021-03-06 16:28:38 +0100

    + separate class for managing movement state

---
+++ Source/SpaceMouseReader/Private/MovementState.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovementState.h"
+
+
+FMovementState::FMovementState()
+{
+}
+
+FMovementState::~FMovementState()
+{
+}
+
+void FMovementState::Move()
+{
+    bMoving = true;
+}
+
+void FMovementState::PreTick()
+{
+    bPrevMoving = MovementTimed > 0;
+    bMoving = false;
+}
+
+void FMovementState::Tick(float MovementTimeTolerance, float DeltaSeconds)
+{
+    if(bMoving) MovementTimed = MovementTimeTolerance;
+    
+    bOnMovementStartedFrame = MovementTimed > 0 && !bPrevMoving;
+    if(bOnMovementStartedFrame) OnMovementStarted.Broadcast();
+    
+    MovementTimed -= DeltaSeconds;
+    
+    bOnMovementEndedFrame = MovementTimed <= 0 && bPrevMoving;
+    if(bOnMovementEndedFrame) OnMovementEnded.Broadcast();
+}
