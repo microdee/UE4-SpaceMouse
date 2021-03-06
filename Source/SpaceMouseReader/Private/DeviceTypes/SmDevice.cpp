fb2d6f4a95790148eeaf0476325c0d51daf3e797
David Morasz <david.morasz@meso.design>
2021-03-07 00:17:09 +0100

    + SmDevice which will eventually replace SpaceMouseDevice

---
+++ Source/SpaceMouseReader/Private/DeviceTypes/SmDevice.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DeviceTypes/SmDevice.h"
+
+
+#include "DebugInfoPrinter.h"
+#include "MovementState.h"
+#include "ProcessedDeviceOutput.h"
+#include "ReadingMethod/DataReadingMethod.h"
+
+FSmDevice::FSmDevice(
+    const FString DeviceName,
+    const ESmModelConfidence ModelConfidence,
+    const TSharedPtr<FButtonCapabilities> Buttons,
+    const TSharedPtr<FDataReadingMethod> DataReadingMethod,
+    const FSmDeviceInstantiation& InstanceData
+)   : DeviceName(DeviceName)
+    , ModelConfidence(ModelConfidence)
+    , InternalID(InstanceData.InternalID)
+    , UserSettings(InstanceData.UserSettings)
+    , Buttons(Buttons)
+    , DataReadingMethod(DataReadingMethod)
+    , HidDevice(InstanceData.HidDevice)
+{
+}
+
+FSmDevice::~FSmDevice()
+{
+}
+
+void FSmDevice::TickInit()
+{
+    if(!bInited)
+    {
+        bInited = true;
+        Data = MakeShared<FProcessedDeviceOutput>();
+        DebugInfoPrinter = MakeShared<FDebugInfoPrinter>([this]() { return UserSettings.bPrintDebug; });
+        MovementState = MakeShared<FMovementState>();
+
+        DataReadingMethod->OnDataReceived.AddLambda([this]()
+        {
+           DebugInfoPrinter->Print(DeviceName, HidDevice->DeviceInfo, InternalID); 
+        });
+    }
+}
+
+void FSmDevice::Tick(float DeltaSeconds)
+{
+    TickInit();
+    
+    FDataReadingOutput Output { Data, DebugInfoPrinter, HidDevice, MovementState, UserSettings };
+    DataReadingMethod->Tick(Output, DeltaSeconds);
+}
