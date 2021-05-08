1eaba0b1f4fe44335513c30b7890e111496c2682
David Morasz <m@mcro.de>
2021-05-08 23:01:38 +0200

    + 3DxWare SDK references

--- Source/SpaceMouseReader/SpaceMouseReader.Build.cs
+++ Source/SpaceMouseReader/SpaceMouseReader.Build.cs
@@ -6,22 +6,37 @@ using System.IO;
 
 public class SpaceMouseReader : ModuleRules
 {
-	public SpaceMouseReader(ReadOnlyTargetRules Target) : base(Target)
-	{
+    public SpaceMouseReader(ReadOnlyTargetRules Target) : base(Target)
+    {
         PrecompileForTargets = PrecompileTargetsType.Any;
         PCHUsage = ModuleRules.PCHUsageMode.NoSharedPCHs;
         bEnableUndefinedIdentifierWarnings = false;
-		CppStandard = CppStandardVersion.Cpp17;
-		
-		PublicDependencyModuleNames.AddRange(new []
-		{
+        CppStandard = CppStandardVersion.Cpp17;
+
+        PublicDependencyModuleNames.AddRange(new []
+        {
             "Core",
             "CoreUObject",
             "Engine",
 
-			"HIDUE"
-			// ... add other public dependencies that you statically link with here ...
-		});
-	}
+            "HIDUE"
+            // ... add other public dependencies that you statically link with here ...
+        });
+        
+        var TDxWareDir = Path.Combine(PluginDirectory, "Source", "ThirdParty", "3DxWareSDK", Target.Platform.ToString());
+        if(File.Exists(Path.Combine(TDxWareDir, "inc", "navlib", "navlib.h")))
+        {
+            PublicDefinitions.Add("WITH_3DX_NAVLIB=1");
+            PublicSystemIncludePaths.Add(Path.Combine(TDxWareDir, "inc"));
+            PublicAdditionalLibraries.AddRange(new []
+            {
+                Path.Combine(TDxWareDir, "lib", Target.Architecture, "TDxNavLib.lib")
+            });
+        }
+        else
+        {
+            PublicDefinitions.Add("WITH_3DX_NAVLIB=0");
+        }
+    }
 }
-	
+    
