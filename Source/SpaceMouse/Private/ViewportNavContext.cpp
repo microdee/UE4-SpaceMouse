91985c369b9f9a5db6e11894be8102da95872aa3
David Morasz <m@mcro.de>
2021-06-20 18:39:53 +0200

    + first theoretical draft of controlling editor viewports with navlib

---
+++ Source/SpaceMouse/Private/ViewportNavContext.cpp
@@ -0,0 +1,348 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ViewportNavContext.h"
+#include "EditorViewportClient.h"
+#include "SEditorViewport.h"
+#include "Engine/Selection.h"
+
+#if WITH_3DX_NAVLIB
+
+FViewportNavContext::FViewportNavContext(FEditorViewportClient* InAssociatedViewport)
+    : FTDxNavContext()
+    , AssociatedVp(InAssociatedViewport)
+{
+}
+
+FViewportNavContext::~FViewportNavContext()
+{
+    FCoreDelegates::OnEndFrame.Remove(EndFrame);
+}
+
+FSceneView* FViewportNavContext::GetSceneView()
+{
+    if(!ViewCached)
+    {
+        FSceneViewFamilyContext ViewFamily(FSceneViewFamily::ConstructionValues(
+            AssociatedVp->Viewport,
+            AssociatedVp->GetScene(),
+            AssociatedVp->EngineShowFlags
+        ));
+        ViewCached = AssociatedVp->CalcSceneView(&ViewFamily);
+    }
+    return ViewCached;
+}
+
+void FViewportNavContext::OnPostOpen()
+{
+    FTDxNavContext::OnPostOpen();
+    EndFrame = FCoreDelegates::OnEndFrame.AddLambda([this]()
+    {
+        ViewCached = nullptr;
+        bHitFrame = false;
+        bSelectionBoundsFrame = false;
+        PrevMotion = Motion.Get();
+    });
+    PivotUser.Set(false);
+}
+
+void FViewportNavContext::Tick(float DeltaSeconds)
+{
+}
+
+bool FViewportNavContext::IsMotionStartedFrame() const
+{
+    const bool CurrMotion = Motion.Get(); 
+    return CurrMotion && (CurrMotion ^ PrevMotion);
+}
+
+bool FViewportNavContext::IsMotionFinishedFrame() const
+{
+    const bool CurrMotion = Motion.Get(); 
+    return PrevMotion && (CurrMotion ^ PrevMotion);
+}
+
+void FViewportNavContext::OnNavlibEndFrame()
+{
+    if(Motion.Get())
+    {
+        if(AllowPerspectiveCameraMoveEvent())
+        {
+            // This is important to trigger PerspectiveCameraMoved event from outside.
+            AssociatedVp->MoveViewportCamera(FVector::ZeroVector, FRotator::ZeroRotator);
+        }
+        AssociatedVp->Viewport->InvalidateHitProxy();
+    }
+}
+
+void FViewportNavContext::OnViewPerspectiveSet(const FViewPerspectiveProperty& InValue)
+{
+    AssociatedVp->SetViewportType(InValue.Get() ? LVT_Perspective : LVT_OrthoFreelook);
+}
+
+void FViewportNavContext::OnViewPerspectiveGet(FViewPerspectiveProperty& InValue)
+{
+    InValue.Set(AssociatedVp->IsPerspective());
+}
+
+void FViewportNavContext::OnViewRotatableGet(FViewRotatableProperty& InValue)
+{
+    InValue.Set(
+        AssociatedVp->GetViewportType() == LVT_Perspective
+        || AssociatedVp->GetViewportType() == LVT_OrthoFreelook
+    );
+}
+
+bool FViewportNavContext::IsViewExtentsAvailable()
+{
+    return AssociatedVp->IsOrtho();
+}
+
+void FViewportNavContext::OnViewExtentsGet(FViewExtentsProperty& InValue)
+{
+    FBox Result({
+        0.0f,
+        -AssociatedVp->GetOrthoZoom() * 0.5f,
+        -AssociatedVp->GetOrthoZoom() * 0.5f
+    }, {
+        AssociatedVp->GetFarClipPlaneOverride(),
+        AssociatedVp->GetOrthoZoom() * 0.5f,
+        AssociatedVp->GetOrthoZoom() * 0.5f
+    });
+    InValue.SetUE(Result);
+}
+
+void FViewportNavContext::OnViewExtentsSet(const FViewExtentsProperty& InValue)
+{
+    AssociatedVp->SetOrthoZoom(InValue.GetUE().GetSize().Y);
+}
+
+bool FViewportNavContext::IsViewFovAvailable()
+{
+    return AssociatedVp->IsPerspective();
+}
+
+void FViewportNavContext::OnViewFovGet(FViewFovProperty& InValue)
+{
+    InValue.Set(FMath::DegreesToRadians(AssociatedVp->ViewFOV));
+}
+
+void FViewportNavContext::OnViewFovSet(const FViewFovProperty& InValue)
+{
+    AssociatedVp->ViewFOV = InValue.Get();
+}
+
+void FViewportNavContext::OnViewAffineGet(FViewAffineProperty& InValue)
+{
+    auto View = GetSceneView();
+    InValue.SetUE(View->ViewMatrices.GetViewMatrix());
+}
+
+void FViewportNavContext::OnViewAffineSet(const FViewAffineProperty& InValue)
+{
+    FTransform InTr(InValue.GetUE());
+    AssociatedVp->SetViewLocation(InTr.GetLocation());
+    AssociatedVp->SetViewRotation(FRotator(InTr.GetRotation()));
+}
+
+void FViewportNavContext::OnViewFrustumGet(FViewFrustumProperty& InValue)
+{
+    auto View = GetSceneView();
+    InValue.SetUE(View->ViewMatrices.GetProjectionMatrix());
+}
+
+void FViewportNavContext::OnPivotPositionSet(const FPivotPositionProperty& InValue)
+{
+    Pivot = InValue.GetUE();
+}
+
+bool FViewportNavContext::IsHitLookatAvailable()
+{
+    CalcHitTest();
+    return bIsHitAvailable;
+}
+
+void FViewportNavContext::OnHitLookatGet(FHitLookatProperty& InValue)
+{
+    CalcHitTest();
+    InValue.SetUE(HitPosition);
+}
+
+void FViewportNavContext::OnSelectionEmptyGet(FSelectionEmptyProperty& InValue)
+{
+    InValue.Set(!IsSelectionAvailable());
+}
+
+bool FViewportNavContext::IsSelectionAffineAvailable()
+{
+    return IsSelectionAvailable();
+}
+
+void FViewportNavContext::OnSelectionAffineGet(FSelectionAffineProperty& InValue)
+{
+    auto Selected = GEditor->GetSelectedActors();
+    if(Selected->Num() == 1)
+    {
+        auto SingleSelected = Selected->GetTop<AActor>();
+        auto ActorTr = SingleSelected->GetTransform();
+        InValue.SetUE(ActorTr.ToMatrixWithScale());
+    }
+    else if(Selected->Num() > 1)
+    {
+        CalcSelectionBounds();
+        FTransform Result((SelectionMin + SelectionMax) * 0.5f);
+        InValue.SetUE(Result.ToMatrixNoScale());
+    }
+}
+
+void FViewportNavContext::OnSelectionAffineSet(const FSelectionAffineProperty& InValue)
+{
+    auto Selected = GEditor->GetSelectedActors();
+    if(Selected->Num() == 1)
+    {
+        auto SingleSelected = Selected->GetTop<AActor>();
+        SingleSelected->SetActorTransform(FTransform(InValue.GetUE()));
+    }
+    else if(Selected->Num() > 1)
+    {
+        TArray<AActor*> Actors;
+        Selected->GetSelectedObjects<AActor>(Actors);
+        for(auto Actor : Actors)
+        {
+            FMatrix ActorTr = Actor->GetTransform().ToMatrixWithScale();
+            Actor->SetActorTransform(FTransform(ActorTr * InValue.GetUE()));
+        }
+    }
+}
+
+bool FViewportNavContext::IsSelectionExtentsAvailable()
+{
+    return IsSelectionAvailable();
+}
+
+void FViewportNavContext::OnSelectionExtentsGet(FSelectionExtentsProperty& InValue)
+{
+    InValue.SetUE({SelectionMin, SelectionMax});
+}
+
+bool FViewportNavContext::IsModelExtentsAvailable()
+{
+    // TODO: treat mesh-editor and other single object editor viewports separately.
+    return false;
+}
+
+void FViewportNavContext::OnPointerPositionGet(FPointerPositionProperty& InValue)
+{
+    auto WorldCursor = AssociatedVp->GetCursorWorldLocationFromMousePos();
+    InValue.SetUE(WorldCursor.GetOrigin());
+}
+
+bool FViewportNavContext::AllowPerspectiveCameraMoveEvent() const
+{
+    static TSet<FName> IncompatibleViewports =
+    {
+        FName("SStaticMeshEditorViewport")
+    };
+
+    FName widgetType = AssociatedVp->GetEditorViewportWidget()->GetType();
+    return !IncompatibleViewports.Contains(widgetType);
+}
+
+bool FViewportNavContext::IsLevelEditorViewport()
+{
+    auto VpType = AssociatedVp->GetEditorViewportWidget()->GetType();
+    return VpType == FName("SLevelEditorViewport");
+}
+
+void FViewportNavContext::CalcHitTest()
+{
+    if(bHitFrame) return;
+    
+    const bool WithAperture = false;
+    float TempLength = 10000;
+    auto Start = HitLookfrom.GetUE();
+    auto Dir = HitDirection.GetUE();
+    auto World = AssociatedVp->GetWorld();
+
+    // TODO: consider HitSelectionOnly
+
+    if(WithAperture)
+    {
+        auto Aperture = HitAperture.GetUE();
+        FLookFromMatrix LookFrom(Start, Dir, GetSceneView()->GetViewUp());
+        TArray<FOverlapResult> Overlaps;
+        FVector MiddlePoint = Start + Dir * TempLength * 0.5f;
+        FQuat MiddleRot = FRotator(90.0f, 0.0f, 0.0f).Quaternion() * LookFrom.ToQuat();
+        bIsHitAvailable = World->OverlapMultiByChannel(
+            Overlaps,
+            MiddlePoint, MiddleRot,
+            ECC_Visibility,
+            FCollisionShape::MakeCapsule(Aperture, TempLength * 0.5f),
+            FCollisionQueryParams(true),
+            FCollisionResponseParams(ECR_Overlap)
+        );
+        if(bIsHitAvailable)
+        {
+            // TODO: get closest overlap and other things
+        }
+    }
+    else
+    {
+        FHitResult HitResult;
+        bIsHitAvailable = World->LineTraceSingleByChannel(
+            HitResult,
+            Start,
+            Start + Dir * TempLength,
+            ECC_Visibility,
+            FCollisionQueryParams(true)
+        );
+        if(bIsHitAvailable)
+        {
+            HitPosition = HitResult.Location;
+        }
+    }
+    bHitFrame = true;
+}
+
+void FViewportNavContext::CalcSelectionBounds()
+{
+    if(bSelectionBoundsFrame) return;
+    
+    auto Selected = GEditor->GetSelectedActors();
+    if(Selected->Num() == 1)
+    {
+        auto SingleSelected = Selected->GetTop<AActor>();
+        FVector Center, Extent;
+        SingleSelected->GetActorBounds(true, Center, Extent, true);
+        SelectionMin = Center - Extent * 0.5f;
+        SelectionMin = Center + Extent * 0.5f;
+    }
+    else if(Selected->Num() > 1)
+    {
+        FVector MinLocation({std::numeric_limits<float>::max()});
+        FVector MaxLocation({std::numeric_limits<float>::min()});
+        
+        TArray<AActor*> Actors;
+        Selected->GetSelectedObjects<AActor>(Actors);
+        for(auto Actor : Actors)
+        {
+            FVector Center, Extent;
+            Actor->GetActorBounds(true, Center, Extent, true);
+            MinLocation = FMath::Min(MinLocation, Center - Extent * 0.5f);
+            MaxLocation = FMath::Max(MaxLocation, Center + Extent * 0.5f);
+        }
+    }
+    bSelectionBoundsFrame = true;
+}
+
+bool FViewportNavContext::IsSelectionAvailable()
+{
+    if(IsLevelEditorViewport())
+    {
+        auto Selected = GEditor->GetSelectedActors();
+        return Selected->Num() > 0;
+    }
+    return false;
+}
+
+#endif
---
+++ Source/SpaceMouse/Public/ViewportNavContext.h
@@ -0,0 +1,92 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Navlib/TDxNavContext.h"
+
+#if WITH_3DX_NAVLIB
+
+class FSceneView;
+/**
+ * 
+ */
+class SPACEMOUSE_API FViewportNavContext : public FTDxNavContext
+{
+public:
+    FViewportNavContext(FEditorViewportClient* InAssociatedViewport);
+    virtual ~FViewportNavContext() override;
+
+    FSceneView* GetSceneView();
+    virtual void OnPostOpen() override;
+    void Tick(float DeltaSeconds);
+
+    bool IsMotionStartedFrame() const;
+    bool IsMotionFinishedFrame() const;
+
+    virtual void OnNavlibEndFrame() override;
+
+    virtual void OnViewPerspectiveSet(const FViewPerspectiveProperty& InValue) override;
+    virtual void OnViewPerspectiveGet(FViewPerspectiveProperty& InValue) override;
+    virtual void OnViewRotatableGet(FViewRotatableProperty& InValue) override;
+    
+    virtual bool IsViewExtentsAvailable() override;
+    virtual void OnViewExtentsGet(FViewExtentsProperty& InValue) override;
+    virtual void OnViewExtentsSet(const FViewExtentsProperty& InValue) override;
+
+    virtual bool IsViewFovAvailable() override;
+    virtual void OnViewFovGet(FViewFovProperty& InValue) override;
+    virtual void OnViewFovSet(const FViewFovProperty& InValue) override;
+
+    virtual void OnViewAffineGet(FViewAffineProperty& InValue) override;
+    virtual void OnViewAffineSet(const FViewAffineProperty& InValue) override;
+    virtual void OnViewFrustumGet(FViewFrustumProperty& InValue) override;
+
+    // I'm not sure yet how restrictive this is according to its description
+    virtual bool IsViewTargetAvailable() override { return false; }
+
+    virtual void OnPivotPositionSet(const FPivotPositionProperty& InValue) override;
+
+    virtual bool IsHitLookatAvailable() override;
+    virtual void OnHitLookatGet(FHitLookatProperty& InValue) override;
+
+    virtual void OnSelectionEmptyGet(FSelectionEmptyProperty& InValue) override;
+
+    virtual bool IsSelectionAffineAvailable() override;
+    virtual void OnSelectionAffineGet(FSelectionAffineProperty& InValue) override;
+    virtual void OnSelectionAffineSet(const FSelectionAffineProperty& InValue) override;
+
+    virtual bool IsSelectionExtentsAvailable() override;
+    virtual void OnSelectionExtentsGet(FSelectionExtentsProperty& InValue) override;
+    
+    // TODO: treat mesh-editor and other single object editor viewports separately.
+    virtual bool IsModelExtentsAvailable() override;
+
+    virtual void OnPointerPositionGet(FPointerPositionProperty& InValue) override;
+    
+protected:
+    FEditorViewportClient* AssociatedVp;
+    bool AllowPerspectiveCameraMoveEvent() const;
+
+    bool IsLevelEditorViewport();
+    void CalcHitTest();
+    void CalcSelectionBounds();
+    bool IsSelectionAvailable();
+    
+private:
+    FSceneView* ViewCached = nullptr;
+    FDelegateHandle EndFrame;
+
+    bool PrevMotion;
+    
+    bool bHitFrame;
+    bool bIsHitAvailable;
+    FVector HitPosition;
+    FVector Pivot;
+
+    bool bSelectionBoundsFrame;
+    FVector SelectionMin;
+    FVector SelectionMax;
+};
+
+#endif
