f47d8f13e7a0f9669febd239a4d1d9b14ba5f64c
David Morasz <m@mcro.de>
2021-06-20 20:10:29 +0200

    + SmEditorManagerBase

---
+++ Source/SpaceMouse/Private/SmEditorManagerBase.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SmEditorManagerBase.h"
+
+#include "Editor.h"
+#include "EditorViewportClient.h"
+#include "SEditorViewport.h"
+#include "SpaceMouseConfig.h"
+#include "Framework/Commands/InputBindingManager.h"
+
+
+FSmEditorManagerBase::FSmEditorManagerBase()
+    : FTickableEditorObject()
+{
+}
+
+FUserSettings FSmEditorManagerBase::GetUserSettings()
+{
+    return GetMutableDefault<USpaceMouseConfig>()->GetUserSettings();
+}
+
+TArray<FEditorViewportClient*> FSmEditorManagerBase::GetAllViewports()
+{
+#if UE_VERSION >= MAKE_UE_VERSION(4, 22)
+    return GEditor->GetAllViewportClients();
+#else
+    return GEditor->AllViewportClients;
+#endif
+}
+
+bool FSmEditorManagerBase::IsViewportValid(FEditorViewportClient* InVp)
+{
+    for (FEditorViewportClient* Cvp : GetAllViewports())
+    {
+        if (Cvp == InVp) return true;
+    }
+    return false;
+}
+
+bool FSmEditorManagerBase::IsActiveViewportInvalid() const
+{
+    return !IsViewportValid(ActiveViewportClient);
+}
+
+void FSmEditorManagerBase::Initialize()
+{
+    FSpaceMouseManager::Initialize();
+    
+    auto& Ibm = FInputBindingManager::Get();
+    Ibm.SaveInputBindings();
+}
+
+void FSmEditorManagerBase::TickManager(float DeltaSecs)
+{
+    FSpaceMouseManager::TickManager(DeltaSecs);
+}
+
+void FSmEditorManagerBase::Tick(float DeltaTime)
+{
+    TickManager(DeltaTime);
+}
+
+void FSmEditorManagerBase::ManageActiveViewport()
+{
+    auto AllViewportClients = GetAllViewports();
+    if(IsActiveViewportInvalid())
+    {
+        ActiveViewportClient = nullptr;
+        OnActiveViewportClosed();
+    }
+    
+    for (FEditorViewportClient* Cvp : AllViewportClients)
+    {
+        if (!Cvp) continue;
+        if (!Cvp->GetEditorViewportWidget().Get()) continue;
+        
+        OnEnumerateViewport(Cvp);
+        
+        if (Cvp->GetEditorViewportWidget().Get()->HasAnyUserFocusOrFocusedDescendants())
+        {
+            if(Cvp == ActiveViewportClient) break;
+            if (Cvp->IsVisible() /* && Cvp->IsPerspective() */)
+            {
+                OnActiveViewportChanged(Cvp, ActiveViewportClient);
+                ActiveViewportClient = Cvp;
+                break;
+            }
+        }
+    }
+}
---
+++ Source/SpaceMouse/Public/SmEditorManagerBase.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpaceMouseManager.h"
+#include "TickableEditorObject.h"
+
+class FEditorViewportClient;
+
+/**
+ * 
+ */
+class SPACEMOUSE_API FSmEditorManagerBase : public FSpaceMouseManager, public FTickableEditorObject
+{
+public:
+    FSmEditorManagerBase();
+    
+protected:
+    
+    FEditorViewportClient* ActiveViewportClient = nullptr;
+    
+    virtual FUserSettings GetUserSettings() override;
+
+    static TArray<FEditorViewportClient*> GetAllViewports();
+    static bool IsViewportValid(FEditorViewportClient* InVp);
+    bool IsActiveViewportInvalid() const;
+
+    virtual void OnActiveViewportChanged(FEditorViewportClient* Current, FEditorViewportClient* Previous) {}
+    virtual void OnActiveViewportClosed() {}
+    virtual void OnEnumerateViewport(FEditorViewportClient* Current) {}
+    
+public:
+    
+    virtual void Initialize() override;
+    virtual void TickManager(float DeltaSecs) override;
+    virtual void Tick(float DeltaTime) override;
+
+    virtual void ManageActiveViewport();
+    
+    virtual TStatId GetStatId() const override { return {}; }
+};
