1fdf1618df8afd99bf325738f06aeb5673f67d81
David Morasz <m@mcro.de>
2021-06-20 21:12:41 +0200

    + first theoretical draft of SmNavlibEditorManager

---
+++ Source/SpaceMouse/Private/SmNavlibEditorManager.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SmNavlibEditorManager.h"
+
+#include "EditorViewportClient.h"
+#include "ViewportNavContext.h"
+
+
+FSmNavlibEditorManager::FSmNavlibEditorManager()
+    : FSmEditorManagerBase()
+{
+}
+
+FSmNavlibEditorManager::~FSmNavlibEditorManager()
+{
+}
+
+void FSmNavlibEditorManager::TickManager(float DeltaSecs)
+{
+    FSmEditorManagerBase::TickManager(DeltaSecs);
+
+    TArray<FEditorViewportClient*> InvalidViewports;
+    for(auto Vp : AllNavContexts)
+    {
+        if(IsViewportValid(Vp.Key)) continue;
+        InvalidViewports.Add(Vp.Key);
+    }
+    for(auto InvalidVp : InvalidViewports)
+    {
+        AllNavContexts.Remove(InvalidVp);
+    }
+    
+    for(auto Vp : AllNavContexts)
+    {
+        Vp.Value->Tick(DeltaSecs);
+    }
+}
+
+void FSmNavlibEditorManager::OnEnumerateViewport(FEditorViewportClient* Current)
+{
+    if(!AllNavContexts.Contains(Current))
+    {
+        auto NewVp = MakeShared<FViewportNavContext>(Current);
+        NewVp->Open();
+        AllNavContexts.Add(Current, NewVp);
+    }
+}
+
+void FSmNavlibEditorManager::OnActiveViewportChanged(FEditorViewportClient* Current, FEditorViewportClient* Previous)
+{
+    if(Current) AllNavContexts[Current]->Focus.Set(true);
+    if(Previous) AllNavContexts[Previous]->Focus.Set(false);
+}
---
+++ Source/SpaceMouse/Public/SmNavlibEditorManager.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SmEditorManagerBase.h"
+#include "TickableEditorObject.h"
+
+class FViewportNavContext;
+
+/**
+ * 
+ */
+class SPACEMOUSE_API FSmNavlibEditorManager : public FSmEditorManagerBase
+{
+public:
+    FSmNavlibEditorManager();
+    virtual ~FSmNavlibEditorManager() override;
+protected:
+    virtual void TickManager(float DeltaSecs) override;
+
+    virtual void OnEnumerateViewport(FEditorViewportClient* Current) override;
+    virtual void OnActiveViewportChanged(FEditorViewportClient* Current, FEditorViewportClient* Previous) override;
+private:
+    
+    TMap<FEditorViewportClient*, TSharedPtr<FViewportNavContext>> AllNavContexts;
+};
